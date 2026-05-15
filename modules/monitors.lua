ab8809b80d58eab9a0c9ed42568012e9d78e7ff1
Addison Blais <calvinseikoblais@gmail.com>
2026-05-14 22:30:32 -0400

    Added explicit desktop monitors dp-1 and hdmi-a-1

--- modules/monitors.lua
+++ modules/monitors.lua
@@ -4,3 +4,17 @@ hl.monitor({
 	position  = "0x0";
 	scale     = "1";
 })
+
+hl.monitor({
+	output	  = "DP-1";
+	mode	  = "2560x1440@164.96";
+	position  = "1920x0";
+	scale     = "1";
+})
+
+hl.monitor({
+	output	  = "HDMI-A-1",
+	mode	  = "1920x1080@180";
+	position  = "0x360";
+	scale     = "1";
+})
