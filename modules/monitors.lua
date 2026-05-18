a943b1d44853bc29e2177ee75d3f215877b74e82
addiGoat <calvinseikoblais@gmail.com>
2026-05-17 21:42:47 -0400

    added config for drawing tablet in monitors.lua

--- modules/monitors.lua
+++ modules/monitors.lua
@@ -1,20 +1,31 @@
-hl.monitor({
-	output	  = "";
-	mode	  = "2560x1600@165";
-	position  = "0x0";
-	scale     = "1";
-})
+local env = require("modules.env")
 
-hl.monitor({
-	output	  = "DP-1";
-	mode	  = "2560x1440@164.96";
-	position  = "1920x0";
-	scale     = "1";
-})
+if env.is_desktop then
+	hl.monitor({
+		output = "DP-1",
+		mode = "2560x1440@164.96",
+		position = "1920x0",
+		scale = "1",
+	})
 
-hl.monitor({
-	output	  = "HDMI-A-1",
-	mode	  = "1920x1080@180";
-	position  = "0x360";
-	scale     = "1";
-})
+	hl.monitor({
+		output = "HDMI-A-1",
+		mode = "1920x1080@180",
+		position = "0x360",
+		scale = "1",
+	})
+else
+	hl.monitor({
+		output = "eDP-1",
+		mode = "2560x1600@165",
+		position = "0x0",
+		scale = "1",
+	})
+
+	hl.monitor({
+		output = "HDMI-A-1",
+		mode = "1920x1080@60",
+		position = "2560x0",
+		scale = "1",
+	})
+end
