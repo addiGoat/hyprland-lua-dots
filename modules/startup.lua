016a2cd878400a21a98b3e5608818782c60a25d9
addiGoat <calvinseikoblais@gmail.com>
2026-05-17 21:36:06 -0400

    added mpvpaper and background switcher test

--- modules/startup.lua
+++ modules/startup.lua
@@ -7,12 +7,16 @@ FileExplorer = "nautilus"
 	hl.env("HYPRCURSOR_SIZE", "24")
 	hl.env("HYPRCURSOR_THEME", "moga")
 	hl.env("XCURSOR_SIZE", "24")
+	hl.env("XCURSOR_THEME", "moga")
 	hl.env("TERMINAL", "kitty")
 	hl.env("EDITOR", "nvim")
 
 
 -- AUTOSTART --
 hl.on("hyprland.start", function ()
-	hl.exec_cmd("waybar & hyprpaper")
+	hl.exec_cmd("waybar")
 	hl.exec_cmd("hyprpm reload")
+	hl.exec_cmd(
+		"mpvpaper -o \"--loop panscan=1 input-ipc-server=/tmp/mpv-socket\" ALL ~/Pictures/Wallpapers/NightFox.png"
+	)
 end)
